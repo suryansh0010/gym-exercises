e3723adf5e6670bb6db3093a26ae5e3ae3b20897
suryansh0010 <suryanshawasthi9@gmail.com>
2025-03-02 22:15:08 +0530

    updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from 'react';
+// import { Link } from 'react-router-dom';
+// import { Stack } from '@mui/material';
+// import Logo from '../assets/images/Logo.png';
 
 const Footer = () => {
   return (
     <div>
-      
-    </div>
-  )
-}
+      footer    </div>
+  );
+};
 
-export default Footer
+export default Footer;
