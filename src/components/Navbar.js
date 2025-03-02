aec0a51958dc6060e8b72d4d4cdfc39a5cf3b9d9
suryansh0010 <suryanshawasthi9@gmail.com>
2025-03-02 22:14:57 +0530

    updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { Stack } from '@mui/material';
+import { Link } from 'react-router-dom';
+import Logo from '../assets/images/Logo.png';
 
-const Navbar = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+const Navbar = () => (
+  <Stack>
+    <Link to='/'>
+      <img src={Logo} alt="logo" style={{width:'48px',height:'48px',margin:'0 20px'}}/>
+    </Link>
+    <Stack>
+        <Link to="/" style={{textDecoration:'none',color:'#3A1212',borderBottom:'3px solid #FF2625'}}>Home</Link>
+        <a href='#exercises' style={
+            {textDecoration:'none', color:'#3A1212'
+        }}>Exercises</a>
+    </Stack>
+  </Stack>
+);
 
-export default Navbar
+export default Navbar;
