1a84bb3a233a10fb607359756619d9892974f386
suryansh0010 <suryanshawasthi9@gmail.com>
2025-03-03 02:27:39 +0530

    updated

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import { Box } from '@mui/material'
+
+import HeroBanner from '../components/HeroBanner'
+import SearchExercises from '../components/SearchExercises'
+import Exercises from '../components/Exercises'
 
 const Home = () => {
   return (
-    <div>
-      
-    </div>
+    <Box>
+      <HeroBanner />
+      <SearchExercises />
+      <Exercises/>
+    </Box>
   )
 }
 
