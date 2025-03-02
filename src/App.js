9af33096238681432dac7a8f571fa4e4a292475f
suryansh0010 <suryanshawasthi9@gmail.com>
2025-03-02 20:32:01 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
 const App = () => {
-  return <Box>
+  return <Box width="400px" sx={{width:{xl: '1488px'}}} m='auto'>
     <Navbar/>
     <Routes>
         <Route path='/' element={<Home/>} />
