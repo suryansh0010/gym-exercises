8041842bb3ee032e12e7315154dac48e6315e87a
suryansh0010 <suryanshawasthi9@gmail.com>
2025-03-04 01:51:48 +0530

    updated

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,29 @@
-import React from 'react'
-import { Box } from '@mui/material'
+import React, { useState } from 'react'; // Removed duplicate import
+import { Box } from '@mui/material';
 
-import HeroBanner from '../components/HeroBanner'
-import SearchExercises from '../components/SearchExercises'
-import Exercises from '../components/Exercises'
+import HeroBanner from '../components/HeroBanner';
+import SearchExercises from '../components/SearchExercises';
+import Exercises from '../components/Exercises';
 
 const Home = () => {
+  const [bodyPart, setBodyPart] = useState('all');
+  const [ setExercises] = useState([]);
+
   return (
     <Box>
       <HeroBanner />
-      <SearchExercises />
-      <Exercises/>
+      <SearchExercises 
+        setExercises={setExercises} 
+        bodyPart={bodyPart} 
+        setBodyPart={setBodyPart} 
+      />
+      <Exercises 
+        setExercises={setExercises} 
+        bodyPart={bodyPart} 
+        setBodyPart={setBodyPart} 
+      />
     </Box>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
