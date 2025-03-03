410d0b360f6160a6a5adfafad8b70d97a5bef2ef
suryansh0010 <suryanshawasthi9@gmail.com>
2025-03-04 01:51:03 +0530

    BodyParts

---
+++ src/components/BodyPart.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Stack} from '@mui/material';
+import Icon from '../assets/icons/gym.png'
+
+
+const BodyPart = () => {
+  return (
+    <Stack>
+      <img src={Icon} alt=''/>
+    </Stack>
+  )
+}
+
+export default BodyPart
