e133543ffa2ae83afc993ab7dd8d92c8f0afde35
suryansh0010 <suryanshawasthi9@gmail.com>
2025-03-03 21:52:29 +0530

    searchExercises Design

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import {Box, Stack,Button,Typography,TextFeild} from '@mui/material';
 
 const SearchExercises = () => {
   return (
-    <div>
+    <Stack alignItems='center' mt='37px' justifyContent='center' p='20px'>
+        <Typography fontWeight={700} sx={{fontSize:{lg:'44px',xs:'30px'}}} mb='50px' textAlign='center'>
+            Awesome Exercises You <br />
+            Should Know
+        </Typography>
       
-    </div>
+    </Stack>
   )
 }
 
