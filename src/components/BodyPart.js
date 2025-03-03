7ac9fbd47e72c0fdf0943bdd22f5a05067b5f329
suryansh0010 <suryanshawasthi9@gmail.com>
2025-03-04 02:52:52 +0530

    updated

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -3,10 +3,18 @@ import {Stack} from '@mui/material';
 import Icon from '../assets/icons/gym.png'
 
 
-const BodyPart = () => {
+const BodyPart = ({item,setBodyPart,BodyPart}) => {
   return (
-    <Stack>
-      <img src={Icon} alt=''/>
+    <Stack 
+      type="button"
+      alignItems='center'
+      justifyContent="center"
+      className='bodyPart-card'
+      sx={{
+
+      }}
+    >
+      <img src={Icon} alt='dumbbell' style={{width:'40px',height:'40px'}}/>
     </Stack>
   )
 }
