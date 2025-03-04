22a8514d5f1bf0beca1ef9e44b8e2e764e9db5a8
suryansh0010 <suryanshawasthi9@gmail.com>
2025-03-04 23:41:10 +0530

    updated

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,10 +1,24 @@
 import React from 'react'
+// import Pagination from '@mui/material'
+import {Box,Stack,Typography} from '@mui/material'
+// import {exerciseOprations,fetchData} from '../utils/fetchData'
 
-const Exercises = () => {
+const Exercises = ({exercises,setExercises,bodyPart}) => {
+  console.log(exercises);
   return (
-    <div>
-      
-    </div>
+    <Box id="exercises"
+    sx={{mt:{lg:'110px'}}}
+    mt='50px'
+    p='20px'>
+      <Typography variant='h4' mb='46px'>
+        showing Results
+      </Typography>
+      <Stack direction='row' sx={{gap:{lg:'110px',xs:'50px'}}}
+      flexWrap='wrap' justifyContent='center'>
+      {exercises.map((exercise,index)=>(<p>exercise.name</p>))}
+
+      </Stack>
+    </Box>
   )
 }
 
