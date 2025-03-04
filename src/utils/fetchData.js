bbed4bf149d7d0d3730eb3a0368ec6f431353a77
suryansh0010 <suryanshawasthi9@gmail.com>
2025-03-05 00:51:47 +0530

    updated

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -11,7 +11,7 @@ export const exerciseOptions = {
     method: 'GET',
     headers: {
       'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
-      'X-RapidAPI-Key': 'f0021db587msh781fb1cbef39856p11c183jsn45521d5d1c85',
+      'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
     },
   };
   
