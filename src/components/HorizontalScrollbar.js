fbf6b59c72e7b21b6ac0091c31801496ec620147
suryansh0010 <suryanshawasthi9@gmail.com>
2025-03-04 09:08:15 +0530

    scroll updated

--- src/components/HorizontalScrollbar.js
+++ src/components/HorizontalScrollbar.js
@@ -1,12 +1,18 @@
-import React from "react";
-import { Box } from "@mui/material";
-import BodyPart from "./BodyPart";
+import React from 'react';
+import { Box } from '@mui/material';
+import BodyPart from './BodyPart';
 
 const HorizontalScrollbar = ({ data, bodyPart, setBodyPart }) => {
   return (
-    <Box sx={{ display: "flex", overflowX: "auto", p: 2, scrollBehavior: "smooth", whiteSpace: "nowrap" }}>
+    <Box sx={{ display: 'flex', overflowX: 'auto', p: 2 }}>
       {data.map((item) => (
-        <Box key={item.id || item} itemId={item.id || item} title={item.id || item} m="0 40px" sx={{ flex: "0 0 auto" }}>
+        <Box
+          key={item.id || item}
+          itemId={item.id || item}
+          title={item.id || item}
+          m="0 40px"
+          sx={{ flex: '0 0 auto' }} 
+        >
           <BodyPart item={item} bodyPart={bodyPart} setBodyPart={setBodyPart} />
         </Box>
       ))}
